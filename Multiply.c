0bd901e40068bd833d89c7afe5764d8f78c3a622
Abhigyan Sen <abhigyansen16@gmail.com>
2022-08-23 13:03:47 +0530

    added Multiply.c

---
+++ Multiply.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{    
+    int a, b, mul;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    mul = a * b;      
+    
+    printf("Sum of %d and  %d is %d", a, b, mul);
+    return 0;
+}
